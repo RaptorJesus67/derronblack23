a32d54a7196d737474090a45302df6a23762c918
RaptorJesus67 <experienceit12@gmail.com>
2016-07-22 14:17:19 -0500

    Removed unwanted directory

--- includes/templates/loginTemplate.php
+++
@@ -1,5 +0,0 @@
-<form action="<?php echo BASE_URI; ?>" method='post' class='loginForm'>
-	<input type='text' name='username' class='inputBox' />
-	<input type='password' name='password' class='inputBox' />
-	<input type='submit' name='loginButton' class='inputButton' />
-</form>
